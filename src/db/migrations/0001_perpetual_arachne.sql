5366447982c72f3985b257df7e0b4bfb53e14d13
TruenaryRaj <raj.truenary@gmail.com>
2025-06-19 13:53:37 +0545

    fix: restore previous migration file

--- src/db/migrations/0001_perpetual_arachne.sql
+++ src/db/migrations/0001_perpetual_arachne.sql
@@ -6,11 +6,4 @@ CREATE TABLE `todo` (
 	CONSTRAINT `todo_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
-CREATE TABLE `user` (
-	`id` int AUTO_INCREMENT NOT NULL,
-	`email` varchar(255),
-	`password` varchar(255),
-	CONSTRAINT `user_id` PRIMARY KEY(`id`)
-);
---> statement-breakpoint
 ALTER TABLE `todo` ADD CONSTRAINT `todo_user_id_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE no action ON UPDATE no action;
